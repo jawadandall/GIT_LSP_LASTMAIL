52c97fcd7ec383d200c079c8d3523284d74ced98
jawadandall <jawadandall@gmail.com>
2024-01-19 22:49:33 -0500

    Large Scale Programming Assignment 1

---
+++ LSP Spring 2024/src/org/howard/edu/lsp/hw1/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+*  Name: Jawad Andall
+*/
+
+package org.howard.edu.lsp.hw1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+	}
+
+}
